4df3e3b4d899c60083fdbe36a1c9301fd93a2d65
Romain Laurence <mw3pumat8@gmail.com>
2023-10-18 21:29:41 +0200

    listing des interventions d'une étape

--- resources/views/fiches/etape.blade.php
+++ resources/views/fiches/etape.blade.php
@@ -31,20 +31,45 @@
             <thead>
                 <tr class="text-neutral-500">
                     <th class="px-5 py-3 text-xs font-medium text-left uppercase">Libellé</th>
+                    <th class="px-5 py-3 text-xs font-medium text-left uppercase">Début</th>
+                    <th class="px-5 py-3 text-xs font-medium text-left uppercase">Fin</th>
+                    <th class="px-5 py-3 text-xs font-medium text-left uppercase">Commentaire</th>
+                    <th class="px-5 py-3 text-xs font-medium text-left uppercase">Intervenant</th>
                     <th class="px-5 py-3 text-xs font-medium text-right uppercase">Action</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-neutral-200">
+                @if (empty($interventions))
                     <tr class="text-neutral-800">
+                        <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">Aucune donnée</td>
+                        <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">Aucune donnée</td>
+                        <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">Aucune donnée</td>
+                        <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">Aucune donnée</td>
                         <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">Aucune donnée</td>
                         <td class="px-5 py-4 text-sm font-medium text-right whitespace-nowrap">
                             <a class="text-blue-600 underline hover:text-blue-700 hover:cursor-not-allowed">Détails</a>
                         </td>
                     </tr>
+                    @else
+                    @foreach ($interventions as $intervention)
+                        <tr class="text-neutral-800">
+                            <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">{{ $intervention->libelle }}</td>
+                            <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">{{ $intervention->date_debut_intervention }}</td>
+                            <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">{{ $intervention->date_fin_intervention }}</td>
+                            <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">{{ $intervention->commentaire }}</td>
+                            <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">{{ $intervention->intervenant->nom }}</td>
+                            <td class="px-5 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                <a class="text-blue-600 underline hover:text-blue-700 hover:cursor-not-allowed">Détails</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
             </tbody>
         </table>
 
     </div>
+
+    @dump($interventions, $intervenantsDispo)
 
     </div>
 </div>
