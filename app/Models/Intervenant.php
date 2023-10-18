d3c6fab756e178a4ee49d77547b9058364650348
Romain Laurence <mw3pumat8@gmail.com>
2023-10-18 23:59:33 +0200

    modélisation relation intervenant prestataire

--- app/Models/Intervenant.php
+++ app/Models/Intervenant.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Intervenant extends Authenticatable
 {
@@ -15,6 +16,11 @@ class Intervenant extends Authenticatable
     public function admin(): BelongsTo
     {
         return $this->belongsTo(Admin::class, 'id');
+    }
+
+    public function getPrestataire(): HasOne
+    {
+        return $this->HasOne(Prestataire::class, 'id_intervenant');
     }
 
     public function intervention(): BelongsTo
