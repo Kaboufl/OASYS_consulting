cf37974d5865399e9cdcefa49cafcc1f63dfb8bd
Kaboufl <71098662+Kaboufl@users.noreply.github.com>
2024-06-14 17:06:15 +0200

    affichage des erreurs du formulaire + backlinks

--- resources/views/fiches/etape.blade.php
+++ resources/views/fiches/etape.blade.php
@@ -3,14 +3,17 @@
 <div class="h-full p-4 justify-self-stretch bg-gray-400 rounded-md row-start-2 col-start-2 flex flex-col items-stretch gap-4">
     <div class="grid grid-rows-3 grid-cols-[6fr_3fr] items-center rounded-md border-2 border-neutral-700 bg-neutral-100 p-4">
         <h1 class="col-start-1 font-black text-2xl text-black">Etape : {{ $etape->libelle }}</h1>
-        <h3 class="col-start-1 font-bold text-black">Projet : {{ $projet->libelle }}</h3>
+        <h3 class="col-start-1 flex flex-row gap-2 font-bold text-black">
+            <span>Projet :</span>
+            <a href="{{ route('admin.projet.projet', ['projet' => $projet->id]) }}">{{ $projet->libelle }}</a>
+        </h3>
         @if ($etape->facture)
                 <a class="col-start-1 justify-self-start flex flex-row align-center gap-2 rounded-full bg-cyan-600 text-white px-4 py-2" href="{{ route('admin.projet.etape.facturer', ['projet' => $projet->id, 'etape' => $etape->id]) }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                       </svg>
-                      
+
 
                     <span>Voir la facture</span>
                 </a>
@@ -24,7 +27,7 @@
                     <span>Facturer l'étape</span>
                 </a>
         @endif
-        
+
         <a class="col-start-2 row-start-1 justify-self-end row-span-3 flex flex-row items-center text-blue-400 hover:underline transition-all" href="">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
@@ -32,6 +35,17 @@
             <span>Modifier</span>
         </a>
     </div>
+
+    @if($errors->any())
+        <div class="w-full h-fit p-2 rounded-md bg-red-300 border-2 border-red-800 text-red-700">
+            <h3>Erreur lors de l'ajout de l'intervention : </h3>
+            <ul class="list-disc pl-4 text-sm">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     <div class="min-w-full h-fit rounded-md border-2 border-neutral-700 bg-neutral-100">
 
@@ -41,12 +55,12 @@
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
-                  
+
                 <span>Ajouter une Intervention</span>
             </button>
         </span>
 
-            
+
         <table class="min-w-full divide-y divide-neutral-700">
             <thead>
                 <tr class="text-neutral-500">
@@ -78,7 +92,9 @@
                             <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">{{ $intervention->commentaire }}
                             </td>
                             <td class="px-5 py-4 text-sm font-medium whitespace-nowrap">
-                                {{ $intervention->intervenant->prestataire ? $intervention->intervenant->getPrestataire->raison_sociale : $intervention->intervenant->prenom . ' ' . $intervention->intervenant->nom }}
+                                <a class="text-blue-500 underline" href="{{ route('admin.intervenant', ['intervenant' => $intervention->intervenant->id]) }}">
+                                    {{ $intervention->intervenant->prestataire ? $intervention->intervenant->getPrestataire->raison_sociale : $intervention->intervenant->prenom . ' ' . $intervention->intervenant->nom }}
+                                </a>
                             </td>
                         </tr>
                     @endforeach
@@ -137,7 +153,7 @@
                         </option>
                     @endforeach
                 </select>
-                    
+
             </label>
 
             <div class="col-span-2 w-full flex flex-row justify-around">
