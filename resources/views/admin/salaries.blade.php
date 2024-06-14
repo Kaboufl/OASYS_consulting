0f3bae7ca661c2ff9bfaa2dc3b55ec20d57dc2c0
Kaboufl <71098662+Kaboufl@users.noreply.github.com>
2024-06-14 17:05:54 +0200

    redisign du rendu des erreurs du formulaire

--- resources/views/admin/salaries.blade.php
+++ resources/views/admin/salaries.blade.php
@@ -7,12 +7,12 @@
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
-                  
+
                 <span>Ajouter un salarié</span>
             </button>
         </div>
 
-        @if($errors->any())
+        {{-- @if($errors->any())
         <ul x-data="{ open: false }" x-bind:class="open ? 'hidden' : ''" class="relative border-2 text-red-600 font-semibold border-red-900 rounded-md bg-red-300 min-w-full p-4 mb-2">
             @foreach($errors->all() as $error)
             <li>{{ $error }}</li>
@@ -21,13 +21,35 @@
                 <span>Fermer</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>            
+                </svg>
             </button>
         </ul>
         @endif
 
+
+        @if($errors->any())
+        <script>
+            window.addEventListener('DOMContentLoaded', () => {
+                    @foreach ($errors->all() as $index => $error)
+                        window.toast('{!! $error !!}', {id: {{ $index }},type: 'danger'});
+                    @endforeach
+                })
+            </script>
+        @endif --}}
+
+        @if($errors->any())
+            <div class="w-full h-fit my-2 p-2 rounded-md bg-red-300 border-2 border-red-800 text-red-700">
+                <h3>Erreur lors de l'ajout du salarié : </h3>
+                <ul class="list-disc pl-4 text-sm">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <div class="min-w-full h-fit rounded-md border-2 border-neutral-700 bg-neutral-100">
-            
+
             <table class="min-w-full divide-y divide-neutral-700">
                 <thead>
                     <tr class="text-neutral-500">
@@ -61,16 +83,16 @@
             </table>
 
             {{ $salaries->links() }}
-        
+
         </div>
 
-    </div>
-    
+    </>
+
     <dialog id="addSalarie" class="min-w-96 w-3/4 h-fit rounded relative overflow-hidden p-8 space-y-4">
         <button onclick="addSalarie.close()" class="absolute right-5 top-5 p-2 rounded-full hover:bg-gray-400 hover:text-white transition-all">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>                  
+            </svg>
         </button>
 
         <h3 class="font-bold text-2xl">Ajouter un salarié</h3>
@@ -93,7 +115,11 @@
                 <span class="block mb-2 text-sm font-medium text-gray-900 ">Mot de passe :</span>
                 <input type="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5    dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </label>
-             
+            <label class="inline-block w-full col-start-2">
+                <span class="block mb-2 text-sm font-medium text-gray-900 ">Confirmer le mot de passe :</span>
+                <input type="password" name="confirmPassword" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5    dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </label>
+
             <div class="col-span-2 w-full flex flex-row justify-around">
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Enregistrer</button>
                 <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" onclick="addSalarie.showModal()">Fermer</button>
