eb5be44070655716f8db53fb346ce99c666f2764
Kaboufl <71098662+Kaboufl@users.noreply.github.com>
2024-06-14 17:07:43 +0200

    correction appel relation

--- resources/views/fiches/intervenant.blade.php
+++ resources/views/fiches/intervenant.blade.php
@@ -3,7 +3,7 @@
 <div class="h-full p-4 justify-self-stretch bg-gray-400 rounded-md row-start-2 col-start-2 flex flex-col items-stretch gap-4">
     <div class="grid grid-rows-3 grid-cols-[6fr_3fr] items-center rounded-md border-2 border-neutral-700 bg-neutral-100 p-4">
         <h1 class="col-start-1 font-black text-2xl text-black">Intervenant : {{ $intervenant->prenom }} {{ $intervenant->nom }}</h1>
-        <h3 class="col-start-1 font-black text-black">{{ $intervenant->prestataire ? 'Prestataire externe (taux horaire : '.$intervenant->prestataire->taux_horaire.'€)' : 'Salarié' }}</h3>
+        <h3 class="col-start-1 font-black text-black">{{ $intervenant->prestataire ? 'Prestataire externe (taux horaire : '.$intervenant->getPrestataire->taux_horaire.'€)' : 'Salarié' }}</h3>
         <h4 class="col-start-1 font-medium text-black">Adresse mail : {{ $intervenant->email }}</h4>
     </div>
 
@@ -16,7 +16,7 @@
         Chef de projet
         @endif
     </div>
-    
+
     @dump($intervenant)
 </div>
 
