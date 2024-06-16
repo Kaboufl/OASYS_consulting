0e933cb352b286cc009959f49a4b341251ed8566
Kaboufl <71098662+Kaboufl@users.noreply.github.com>
2024-06-16 02:13:51 +0200

    correction route

--- resources/views/partials/back/nav.blade.php
+++ resources/views/partials/back/nav.blade.php
@@ -7,7 +7,7 @@
     @if(auth()->user()->admin()->count())
 
         <li>
-            <a href="{{ route('admin.dashboard') }}" class="@if(str_contains(Route::currentRouteName(), 'dashboard')) bg-gray-400 font-bold text-gray-800 @else text-white @endif flex flex-row justify-start gap-4 items-center p-2 px-4 rounded-full hover:bg-gray-600 text-lg transition-all">
+            <a href="{{ route('back.admin.dashboard') }}" class="@if(str_contains(Route::currentRouteName(), 'dashboard')) bg-gray-400 font-bold text-gray-800 @else text-white @endif flex flex-row justify-start gap-4 items-center p-2 px-4 rounded-full hover:bg-gray-600 text-lg transition-all">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10 ml-0">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
